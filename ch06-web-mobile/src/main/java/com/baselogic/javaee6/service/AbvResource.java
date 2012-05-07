1a0e1998dd7986391f1dc829709544e823300f3e
Mick Knutson <mickknutson@gmail.com>
2012-05-07 07:01:59 -0400

    Updated the readme file and removed the working directory for Tomcat as I dont want to version that DIR.

--- ch06-web-mobile/src/main/java/com/baselogic/javaee6/service/AbvResource.java
+++ ch06-web-mobile/src/main/java/com/baselogic/javaee6/service/AbvResource.java
@@ -20,6 +20,7 @@ import java.text.DecimalFormat;
  * @see <a href="http://twitter.com/mickknutson">Twitter: http://twitter.com/mickknutson</a>
  * @see <a href="http://github.com/mickknutson">Git hub: http://github.com/mickknutson</a>
  *
+ * @see <a href="http://www.packtpub.com/authors/profiles/mick-knutson">Packt Author Profile</a>
  * @see <a href="http://www.packtpub.com/java-ee6-securing-tuning-extending-enterprise-applications-cookbook/book">JavaEE 6 Cookbook Packt</a>
  * @see <a href="http://www.amazon.com/Cookbook-securing-extending-enterprise-applications/dp/1849683166">JavaEE 6 Cookbook Amazon</a>
  *
